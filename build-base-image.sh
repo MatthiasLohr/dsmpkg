6c597ab3e719875807c73f19214309c0ca57a23c
Matthias Lohr <mail@mlohr.com>
2018-11-04 17:05:43 +0100

    improvements

--- build-base-image.sh
+++ build-base-image.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-docker build -f Dockerfile.base -t matthiaslohr/dsmpkg-env-base .
+source ./dsminfo 2> /dev/null
+
+docker build -f Dockerfile.base -t matthiaslohr/dsmpkg-env-base --build-arg dsm_version="$dsm_version_default" .
 
