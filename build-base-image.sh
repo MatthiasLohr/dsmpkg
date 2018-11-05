2fe7de4208cd2f486b4e84d1c6cc7d8af2482133
Matthias Lohr <mail@mlohr.com>
2018-11-05 15:41:25 +0100

    improved parametrized platform builds

--- build-base-image.sh
+++ build-base-image.sh
@@ -2,5 +2,5 @@
 
 source ./dsminfo 2> /dev/null
 
-docker build -f Dockerfile.base -t matthiaslohr/dsmpkg-env-base --build-arg dsm_version="$dsm_version_default" .
+docker build -f Dockerfile.base -t matthiaslohr/dsmpkg-env:"$dsm_version_default"-base --build-arg dsm_version="$dsm_version_default" .
 
--- build-platform-image.sh
+++ build-platform-image.sh
@@ -3,5 +3,5 @@
 dsm_platform="$1"
 
 source ./dsminfo 2> /dev/null
-docker build -f Dockerfile.platform -t matthiaslohr/dsmpkg-env-"$dsm_platform" --build-arg dsm_version="$dsm_version_default" --build-arg dsm_platform="$dsm_platform" .
+docker build -f Dockerfile.platform -t matthiaslohr/dsmpkg-env:"$dsm_version_default"-"$dsm_platform" --build-arg dsm_version="$dsm_version_default" --build-arg dsm_platform="$dsm_platform" .
 
