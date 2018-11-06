9ace03ff967897297b77ec804143cd5ac45df9e3
Matthias Lohr <mail@mlohr.com>
2018-11-06 16:42:58 +0100

    support version/platform parameters for package building

--- build-platform-package.sh
+++ build-platform-package.sh
@@ -1,9 +1,43 @@
 #!/bin/bash
 
-dsm_version=6.2
-dsm_platform="$1"
+source ./dsminfo 2> /dev/null
+dsm_version="$dsm_version_default"
+dsm_platform=""
+
+POSITIONAL=()
+while [[ $# -gt 0 ]] ; do
+	case $1 in
+		-v|--version)
+			dsm_version="$2"
+			shift
+			shift
+			;;
+		-p|--platform)
+			dsm_platform="$2"
+			shift
+			shift
+			;;
+		-h|--help)
+			echo "Usage: $0 <parameters...>"
+			echo "Available parameters:"
+			echo "  -v --version   Specify DSM version (default $dsm_version_default)"
+			echo "  -p --platform  Specify DSM platform"
+			echo "  -h --help      Show this help text"
+			exit 0
+			;;
+		*)
+			POSITIONAL+=("$1")
+			shift
+			;;
+	esac
+done
+set -- "${POSITIONAL[@]}"
+
+if [ -z "$dsm_version" ] ; then echo "No DSM version specified! Check $0 --help"; exit 1; fi
+if [ -z "$dsm_platform" ] ; then echo "No DSM platform specified! Check $0 --help"; exit 1; fi
+
 rootdir="$( cd "$(dirname "$0")" ; pwd -P )"
-package_source=`readlink -f $2`
+package_source=`readlink -f $1`
 container="dsmpkg-$dsm_version-$dsm_platform"
 dockercmd="docker exec -it $container"
 buildtmp=`mktemp -d -t dsmpkg-build.XXXXXXXX`
